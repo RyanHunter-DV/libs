ecaccc4dc569a94e4be9249c163e49372336f7d4
RyanHunter-DV <86789670+RyanHunter-DV@users.noreply.github.com>
2022-02-09 10:56:14 +0800

    Update __init__.py
    add API::newFile

--- swlibs/pylib/fileUtils/__init__.py
+++ swlibs/pylib/fileUtils/__init__.py
@@ -14,10 +14,25 @@ def getContents(name): #{
 	
 	fh = open(name,mode='r');
 	rCnts = fh.readlines();
+	fh.close();
 	pCnts = [];
 	for cnt in rCnts:
 		pCnts.append(cnt.rstrip('\n'));
 		
 	return pCnts;
 	
+#}
+
+## generate a new file with cnts, if cnts==None, generate an empty file
+## by using this API, file will be overwritten even if it exists.
+def newFile(name,cnts=None): #{
+	fh = open(name,mode='w');
+	if cnts==None:
+		fh.flush();
+	else: #{
+		for cnt in cnts:
+			fh.write(cnt+'\n');
+	#}
+	fh.close();
+	return;
 #}
