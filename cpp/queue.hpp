1ad5ac7d3db58aef0ddec07a23b1f63779fc4ed9
RyanHunter-DV <86789670+RyanHunter-DV@users.noreply.github.com>
2022-02-22 22:51:20 +0800

    internal submit

--- cpp/queue.hpp
+++ cpp/queue.hpp
@@ -7,10 +7,13 @@
 template <class T>
 class queue {
 public:
-	queue<T>() {
+	queue() {
 		elements = new T[1];
 		size = 0;
 		containerSize = 1;
+	}
+	~queue() {
+		delete elements;
 	}
 	void pushBack(T item);
 	// void pushBack(T* item);
@@ -25,10 +28,10 @@ private:
 	// indicates current available memory space of elements[]
 	unsigned int containerSize;
 	// return current containerSize
-	unsigned int currentCanContain();
+	unsigned int currentCanContain() { return containerSize; }
 	// to allocate more size of elements[] according to arg::s specified
 	//
-	void allocateMoreContainer(uint32_t s = 1);
+	void allocateMoreContainer(uint32_t s);
 	// shift the latter one to its previous one, one by one to loop through the
 	// current queue
 	// 1->0,2->1,...,size-1->size-2
@@ -39,14 +42,14 @@ template <class T>
 T queue<T>::popFront() { // {{{
 	if (size <= 0) { return NULL; }
 	T item = *elements;
-	this.shiftToFront();
+	shiftToFront();
 	size--;
 	return item;
 } // }}}
 
 template <class T>
 void queue<T>::shiftToFront() { // {{{
-	for (int i = 0; i < this.size - 1; i++) {
+	for (int i = 0; i < size - 1; i++) {
 		*(elements + i) = *(elements + i + 1);
 	}
 	return;
@@ -64,7 +67,7 @@ void queue<T>::pushBack(T item) // {{{
 } // }}}
 
 template <class T>
-void queue<T>::allocateMoreContainer(uint32_t s = 1) // {{{
+void queue<T>::allocateMoreContainer(uint32_t s) // {{{
 {
 	T* newEles = new T[containerSize + s];
 	if (memCopy(elements, newEles, size)) {
@@ -77,6 +80,7 @@ void queue<T>::allocateMoreContainer(uint32_t s = 1) // {{{
 	containerSize += s; // update containerSize
 } // }}}
 
+using intQueue = queue<int>;
 
 
 #endif
