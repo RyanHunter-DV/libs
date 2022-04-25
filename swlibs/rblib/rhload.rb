b8d27e5b81ae64aebe25d88b553f7073a00a1632
CentOS8 <ryan@192.168.121.129>
2022-04-25 05:45:06 -0700

    add rhload.rb

---
+++ swlibs/rblib/rhload.rb
@@ -0,0 +1,25 @@
+## rhload.rb
+## this lib is for loading *.rh files in rhFlow, which let users to load a file
+## without the extension, for example
+## rhload 'path/file' can load path/file.rh
+## beside, this method can automatically add the path of that file into search path
+def rhload fname; ##{
+
+	extPtrn = /\.rh/;
+
+	if not extPtrn =~ fname
+		fname += '.rh';
+	end
+
+	if File.exists?(fname)
+		load fname;
+	else
+		puts "Error, file(#{fname}) not exists";
+		return;
+	end
+
+	## push dir to LOAD_PATH
+	dir = File.dirname(File.absolute_path(fname));
+	$LOAD_PATH << dir unless $LOAD_PATH.include?(dir);
+	
+end ##}
