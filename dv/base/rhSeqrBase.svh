ca4776164ca611d78a2a7734a5e154a6a870e941
ryanhunter <ryanhunter@rhTestPC.dynamic>
2023-01-15 07:26:09 -0500

    add base seqr

---
+++ dv/base/rhSeqrBase.svh
@@ -0,0 +1,13 @@
+`ifndef rhSeqrBase__svh
+`define rhSeqrBase__svh
+
+class RhSeqrBase #(type REQ=uvm_sequence_item,RSP=REQ) extends uvm_sequence #(REQ,RSP);
+	
+	`uvm_object_utils(RhSeqrBase#(REQ,RSP));
+
+	function new(string name = "RhSeqrBase");
+		super.new(name);
+	endfunction
+	
+endclass
+`endif
