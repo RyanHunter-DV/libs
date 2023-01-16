60aa2563ec7821c1d519409a8b84d3169c53d135
RyanHunter-DV <86789670+RyanHunter-DV@users.noreply.github.com>
2023-01-16 10:30:38 +0800

    Update rhSeqrBase.svh

--- dv/base/rhSeqrBase.svh
+++ dv/base/rhSeqrBase.svh
@@ -1,7 +1,7 @@
 `ifndef rhSeqrBase__svh
 `define rhSeqrBase__svh
 
-class RhSeqrBase #(type REQ=uvm_sequence_item,RSP=REQ) extends uvm_sequence #(REQ,RSP);
+class RhSeqrBase #(type REQ=uvm_sequence_item,RSP=REQ) extends uvm_sequencer #(REQ,RSP);
 	
 	`uvm_object_utils(RhSeqrBase#(REQ,RSP));
 
